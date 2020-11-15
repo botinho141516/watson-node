b669c168af39b20c6f3bc9a703310c2b6fed8e65
Nicolas Salgado <nsalgado@yoobot.com.br>
2020-11-15 17:24:31 -0300

    Audios on gitignore

--- src/database/tables/Audios.js
+++ src/database/tables/Audios.js
@@ -1,9 +1,9 @@
 export const create = `
       CREATE TABLE IF NOT EXISTS Audios (
-      ID int NOT NULL AUTO_INCREMENT,
+      id int NOT NULL AUTO_INCREMENT,
       text varchar(300) NOT NULL,
       audio_path varchar(50) NOT NULL,
-      PRIMARY KEY (ID)
+      PRIMARY KEY (id)
     );`;
 
 
